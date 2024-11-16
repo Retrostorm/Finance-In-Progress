81af278300bac62bd8d97bf824ce0d0f677aa92a
Noam Hazan <noam.hazan01@gmail.com>
2024-11-16 17:17:03 -0600

    nav mobile done

---
+++ index.js
@@ -0,0 +1,15 @@
+function toggleNavOn() {
+  document.getElementById("nav-button-container").style.transform = "translateX(0)"
+}
+
+function toggleNavOff() {
+  document.getElementById("nav-button-container").style.transform = "translateX(100%)"
+}
+
+addEventListener("resize", () => {
+  if(window.innerWidth >= 750) {
+    toggleNavOn()
+  } else {
+    toggleNavOff()
+  }
+});
