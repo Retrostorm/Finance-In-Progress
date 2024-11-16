7ecbe226816a6782d5c223c44891545469ff052f
Retrostorm <126016563+Retrostorm@users.noreply.github.com>
2024-11-16 17:18:56 -0600

    Add files via upload

---
+++ budgetGame.js
@@ -0,0 +1,40 @@
+// Robert Palmer            11-16-24
+// function to act as main function for the budget game
+
+let month = 1;
+let promptInMonth = 0;
+const totalMonths = 6;
+let funds = 1000;
+const income = 13000;
+const fixedExpenses = 1000;
+let thisMonthsPrompts = [];
+const situations = [{"Prompt1": {"Choice": "Feedback"}, "Prompt2": {"Choice": "Feedback"}}];
+let feedback = []; 
+
+function userInput(choice) {
+    // This is the function called when a user gives input to a month / presses one of the buttons
+    
+    // Want new index of feedback to be last index + 1 / length of the array
+    let newIndex = feedback.length;
+
+    // Assign newIndex of feedback to the feedback associated with that month, the current prompt, and the user's choice
+    feedback[newIndex] = situations[month][thisMonthsPrompts[promptInMonth]][choice];
+
+    if (promptInMonth == 2) {
+        nextMonth();
+    } else {
+        nextPrompt();
+    }
+}
+
+function nextPrompt() {
+    thisMonthsPrompts++;
+    
+    // Display stuff
+}
+
+function nextMonth() {
+    month++;
+
+    // Display the next months stuff
+}
