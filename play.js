cbdc2091e9749506f70b49923f3fd6259bdbcc89
Retrostorm <126016563+Retrostorm@users.noreply.github.com>
2024-11-17 01:33:51 -0600

    Update play.js

--- play.js
+++ play.js
@@ -1,10 +1,27 @@
 function nextCard() {
-  document.querySelector(".card:nth-child(1)").style.transform = "translateY(-200%)"
-  document.querySelector(".card:nth-child(1)").style.opacity = ".8"
-  setTimeout(() => {
-    document.querySelector(".card:nth-child(1)").remove()
-  }, 500);  
+    document.querySelector(".card:nth-child(1)").style.transform = "translateY(-200%)";
+    document.querySelector(".card:nth-child(1)").style.opacity = ".8";
+    setTimeout(() => {
+      document.querySelector(".card:nth-child(1)").remove()
+    }, 500);  
+  }
+
+var randWT = (Math.floor(Math.random() * 3) + 1);
+document.querySelector(".card:nth-child(1)").innerHTML = `<img src="/docs/w2-${randWT}.svg" />`;
+
+const wtWages = [2468.54, 64543.84, 13458.21];
+var balance = wtWages[randWt - 1];
+
+// This variable represents costs accrued due to using a credit card, this is displayed at end
+var creditCardBill = 0;
+
+const cards = {1: "AAAAAAAAAA"}
+
+function userChoice(cardNum) {
+
 }
 
-var randWT = (Math.floor(Math.random() * 3) + 1)
-document.querySelector(".card:nth-child(1)").innerHTML = `<img src="/docs/w2-${randWT}.svg" />`
+function balanceChange(change) {
+    // change is either positive for saving or negative for spending
+    balance += change;
+}
