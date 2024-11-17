d4faaedecb79f74a4d4eabcf171936eb8074a586
Retrostorm <126016563+Retrostorm@users.noreply.github.com>
2024-11-16 18:12:58 -0600

    Update budgetGame.js so uses new data structure

--- budgetGame.js
+++ budgetGame.js
@@ -1,9 +1,9 @@
 // Robert Palmer            11-16-24
 // function to act as main function for the budget game
 
-let month = 1;
+let month = 0;
 let promptInMonth = 0;
-const totalMonths = 6;
+const totalMonths = 5;
 let funds = 1000;
 const income = 13000;
 const fixedExpenses = 1000;
@@ -18,7 +18,8 @@ function userInput(choice) {
     let newIndex = feedback.length;
 
     // Assign newIndex of feedback to the feedback associated with that month, the current prompt, and the user's choice
-    feedback[newIndex] = situations[month][thisMonthsPrompts[promptInMonth]][choice];
+    // feedback[newIndex] = situations[month][thisMonthsPrompts[promptInMonth]][choice];
+    feedback[newIndex] = situations[month][promptInMonth]["feedback"][choice]
 
     if (promptInMonth == 2) {
         nextMonth();
