907dc9720444ce65a549986df74b302061ac917d
Retrostorm <126016563+Retrostorm@users.noreply.github.com>
2024-11-17 02:32:28 -0600

    Update play.js

--- play.js
+++ play.js
@@ -4,7 +4,7 @@ function nextCard() {
     setTimeout(() => {
       document.querySelector(".card:nth-child(1)").remove()
     }, 500);  
-  }
+}
 
 var randWT = (Math.floor(Math.random() * 3) + 1);
 document.querySelector(".card:nth-child(1)").innerHTML = `<img src="/docs/w2-${randWT}.svg" />`;
@@ -15,13 +15,24 @@ var balance = wtWages[randWt - 1];
 // This variable represents costs accrued due to using a credit card, this is displayed at end
 var creditCardBill = 0;
 
-const cards = {1: "AAAAAAAAAA"}
+// Data structure for data of cards
+// Individual items are for choice 0 or choice 1
+// First array in each element holds what change takes place, second is value of that change
+const cards = [
+    [["nothing", "nothing"], [0, 0], "Context Here for W2"],
+    [["balance", "balance"], [500, -500], ""], 
+    [["balance", "nothing"], [-2800, 0], ""],
+    [["balance", "nothing"], [-690, 0], ""],
+    [["balance", "credit"], [-75, 75], ""],
+    [["balance", "nothing"], [-68, 0], ""]
+];
 
-function userChoice(cardNum) {
+function userChoice(cardNum, choice) {
+    let card = cards[cardNum];
 
-}
-
-function balanceChange(change) {
-    // change is either positive for saving or negative for spending
-    balance += change;
+    if (card[0][choice] == "balance") {
+        balance += card[1][choice];
+    } else if (card[0][choice] == "credit") {
+        credit += card[1][choice];
+    }
 }
